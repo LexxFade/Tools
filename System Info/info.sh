ad0149300c5a5e4817fbcc780a665d458f8cb0ec
Sasha Scarlett <74601193+LexxFade@users.noreply.github.com>
2020-12-01 08:59:41 +0300

    fixed indentation

--- System Info/info.sh
+++ System Info/info.sh
@@ -10,13 +10,13 @@ while IFS= read -r line
 do
   if [ $index -eq $(( selected + 1 )) ]; then
     echo "  $line OS..............: Arch Linux"
-	elif [ $index -eq $(( selected + 2 )) ]; then
+  elif [ $index -eq $(( selected + 2 )) ]; then
     echo "  $line WM..............: i3wm + gaps"
-	elif [ $index -eq $(( selected + 3 )) ]; then
+  elif [ $index -eq $(( selected + 3 )) ]; then
     echo "  $line Kernel..........: $kernel"
-	elif [ $index -eq $(( selected + 4 )) ]; then
+  elif [ $index -eq $(( selected + 4 )) ]; then
     echo "  $line Uptime..........: $uptime"
-	elif [ $index -eq $(( selected + 5 )) ]; then
+  elif [ $index -eq $(( selected + 5 )) ]; then
     echo "  $line Packages........: $packages"
   fi
   ((index = index + 1))
